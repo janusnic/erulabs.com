60e9fac47ef72fb441a68c543b4c5aea11bb0d02
erulabs <seandon.mooy@gmail.com>
2015-04-13 02:10:35 -0500

    post gulp tutorial

--- src/index.js
+++ src/index.js
@@ -1,68 +1,94 @@
 /* global document, window */
 'use strict';
 
-// A list of posts.
-// To improve caching, we'll load this from a separate file via an HTTP call.
-// That way this file doesn't need to change when posting new blog posts.
-// And therefore, can stay cached much longer.
-let postList = [];
-
 const angular = require('angular');
 const Prism = require('./vendor/prism.js');
 
 const ng = angular.module('erulabs', []);
 
 ng.controller('BlogCtrl', function ($scope, $document, $http, $sce) {
-  $scope.posts = [];
+  // To improve caching, we'll load this from a separate file via an HTTP call.
+  // That way this file doesn't need to change when posting new blog posts.
+  // And therefore, can stay cached much longer.
+  $scope.posts = {};
   $scope.foundEnd = false;
   let postsShown = 0;
   let loading = false;
 
   $scope.urlize = function urlize (string) {
     return string
-      .replace(/ /g, '_')
-      .replace(/(\!|\"|\')/g, '');
+      .replace(/ /g, '_');
+  };
+  $scope.unurlize = function urlize (string) {
+    return string
+      .replace(/_/g, ' ');
   };
 
-  function loadPost (postObj, position) {
-    const id = $scope.urlize(postObj.name);
-    if (position === undefined) {
-      position = $scope.posts.length;
+
+  function loadPost (postObj, callback) {
+    if (typeof postObj === 'string') {
+      postObj = { name: postObj };
     }
+    const id = postObj.name;
+    const canon = $scope.unurlize(postObj.name);
     loading = true;
     $http.get(`/assets/posts/${id}.html`).success(function (data, status, headers) {
       if (status === 200) {
         let date = headers()['last-modified'].split(' ');
-        $scope.posts[position] = {
-          title: postObj.name,
-          date: `${date[2]} ${date[1]}, ${date[3]}`,
-          categories: postObj.categories,
-          html: $sce.trustAsHtml(data)
-        };
+        $scope.postList[id].title = canon;
+        if ($scope.postList[id].date === undefined) {
+          $scope.postList[id].date = `${date[2]} ${date[1]}, ${date[3]}`;
+        }
+        $scope.postList[id].html = $sce.trustAsHtml(data);
+        $scope.postList[id].visible = true;
         setTimeout(function () {
           const eles = document.getElementById(id).getElementsByTagName('pre');
           for (let i = 0; i < eles.length; i++) {
             Prism.highlightElement(eles[i]);
           }
         }, 0);
+        if (callback) {
+          callback();
+        }
         loading = false;
+      } else if (status === 404) {
+        console.log('404');
+      } else {
+        console.log(status);
       }
     });
   }
 
   function loadNextPosts (n) {
     if (n === undefined) { n = 1; }
+    const keys = Object.keys($scope.postList);
     for (let i = 0; i < n; i++) {
-      const next = postList[postsShown];
+      const next = keys[postsShown];
       if (next) {
-        loadPost(next, postsShown);
+        loadPost(next);
         postsShown++;
       }
     }
   }
 
+  function searchByCategory (category) {
+    let matches = 0;
+    for(let curr in $scope.postList) {
+      let thisPost = $scope.postList[curr];
+      if (thisPost && thisPost.categories && thisPost.categories.indexOf(category) > -1) {
+        loadPost(curr);
+        matches++;
+      } else {
+        $scope.postList[curr].visible = false;
+      }
+    }
+    if (matches === 0) {
+      $scope.sorry = `No posts match "${category}"`;
+    }
+  }
+
   $http.get('/assets/posts/posts.json').success(function (data) {
-    postList = data.posts;
+    $scope.postList = data.posts;
     window.onscroll = function () {
       let height = Math.max(
         document.body.scrollHeight,
@@ -77,6 +103,20 @@ ng.controller('BlogCtrl', function ($scope, $document, $http, $sce) {
         }
       }
     };
-    loadNextPosts(2);
+
+    let query = window.location.pathname.split('/')[1];
+    if (query) {
+      if ($scope.postList[query]) {
+        loadPost({
+          name: query
+        }, 0, function () {
+          console.log('ok i loaded it');
+        });
+      } else {
+        searchByCategory(query);
+      }
+    } else {
+      loadNextPosts(2);
+    }
   });
 });
